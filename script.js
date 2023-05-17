e477a8b8cc9013eb68c3dd23ec28df3b5801a364
AlenaDvo <131749753+AlenaDvo@users.noreply.github.com>
2023-05-17 12:03:41 +0200

    Functional version 1

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 // add
 $('button').on('click', () => {
     let newToDo = document.querySelector('input').value;
-    console.log(input.value);
-        
-    
-    $('task-container').append(
-        '<div class="todo-text">'
-        + newToDo
-        + '</div>'
-        // + '<img class="delete" src="\delete_icon.svg" alt="delete" onclick="">'
-        // + '<img class="done" src="\done_icon.svg" alt="done" onclick="lineThrough(this)">'
+    // console.log(input.value);
+
+    $('.task-container').after('<div class="task-container">'+'<div class="todo-text">'+newToDo+'</div>'
+        + '<img class="delete" src="\delete_icon.svg" alt="delete" onclick="">'
+        + '<img class="done" src="\done_icon.svg" alt="done" onclick="lineThrough(this)">'
     );
     console.log('i have been clicked');
     // +'<img class="delete" src="\delete_icon.svg" alt="delete"><img class="done" src="\done_icon.svg" alt="done" onclick= "lineThrough(this)">'
 });
 
 // done
-$('.done').on('click', () => {
-    $('ul').append('<li onclick= "lineThrough(this)">' + newToDo + '</li>');
-});
+// $('.done').on('click', () => {
+//     $('ul').append('<li onclick= "lineThrough(this)">' + newToDo + '</li>');
+// });
 
 // delete
 
