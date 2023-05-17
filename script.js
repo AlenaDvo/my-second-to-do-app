ec4be3f2dc6417b11fefce28a65ff32366ce14af
AlenaDvo <131749753+AlenaDvo@users.noreply.github.com>
2023-05-17 11:40:19 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// add
+$('button').on('click', () => {
+    let newToDo = document.querySelector('input').value;
+    console.log(input.value);
+        
+    
+    $('task-container').append(
+        '<div class="todo-text">'
+        + newToDo
+        + '</div>'
+        // + '<img class="delete" src="\delete_icon.svg" alt="delete" onclick="">'
+        // + '<img class="done" src="\done_icon.svg" alt="done" onclick="lineThrough(this)">'
+    );
+    console.log('i have been clicked');
+    // +'<img class="delete" src="\delete_icon.svg" alt="delete"><img class="done" src="\done_icon.svg" alt="done" onclick= "lineThrough(this)">'
+});
+
+// done
+$('.done').on('click', () => {
+    $('ul').append('<li onclick= "lineThrough(this)">' + newToDo + '</li>');
+});
+
+// delete
+
+
+function lineThrough(li) {
+    p.style = 'text-decoration: line-through';
+}
