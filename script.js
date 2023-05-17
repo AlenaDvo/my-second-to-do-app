b33522ec58f1e44d311c71a2ee8fada3911c8114
AlenaDvo <131749753+AlenaDvo@users.noreply.github.com>
2023-05-17 14:47:14 +0200

    Functional buttons

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 // add
 $('button').on('click', () => {
     let newToDo = document.querySelector('input').value;
-    // console.log(input.value);
 
-    $('.task-container').after('<div class="task-container">'+'<div class="todo-text">'+newToDo+'</div>'
+    $('.task-container').last().after('<div class="task-container">'+'<div class="todo-text">'+newToDo+'</div>'
         + '<img class="delete" src="\delete_icon.svg" alt="delete" onclick="">'
-        + '<img class="done" src="\done_icon.svg" alt="done" onclick="lineThrough(this)">'
+        + '<img class="done" src="\done_icon.svg" alt="done" onclick="">'
     );
-    console.log('i have been clicked');
-    // +'<img class="delete" src="\delete_icon.svg" alt="delete"><img class="done" src="\done_icon.svg" alt="done" onclick= "lineThrough(this)">'
 });
 
-// done
+// // delete
+$('.tasks-container').on('click', '.delete', (event) => {
+    console.log('deleted');
+    event.target.parentNode.remove();
+    // console.log(this);
+    // $('.tasks-container').delete;
+    // $(event.target).
+    // text('This img was clicked.');
+});
+
 // $('.done').on('click', () => {
 //     $('ul').append('<li onclick= "lineThrough(this)">' + newToDo + '</li>');
 // });
 
-// delete
-
-
-function lineThrough(li) {
-    p.style = 'text-decoration: line-through';
-}
+// done
+$('.tasks-container').on('click', '.done', (event) => {
+    event.target.parentNode.style = 'text-decoration: line-through';
+    // console.log($(event.target));
+    // // ('.todo-text').style = 'text-decoration: line-through';
+    // // console.log($('.todo-text'));
+    // // console.log('done');
+    // // // $(event.target).
+    // // $(event.target).text('This img was clicked.');
+});
