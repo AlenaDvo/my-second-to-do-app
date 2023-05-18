00529b5bdc1184cb6b0800cfe6d3963f4336806d
AlenaDvo <131749753+AlenaDvo@users.noreply.github.com>
2023-05-18 08:05:37 +0200

    Deleted unnecessary comments

--- script.js
+++ script.js
@@ -3,32 +3,19 @@ $('button').on('click', () => {
     let newToDo = document.querySelector('input').value;
 
     $('.task-container').last().after('<div class="task-container">'+'<div class="todo-text">'+newToDo+'</div>'
-        + '<img class="delete" src="\delete_icon.svg" alt="delete" onclick="">'
-        + '<img class="done" src="\done_icon.svg" alt="done" onclick="">'
+        + '<img class="delete" src="\delete_icon.svg" alt="delete">'
+        + '<img class="done" src="\done_icon.svg" alt="done">'
     );
 });
 
-// // delete
+// delete
 $('.tasks-container').on('click', '.delete', (event) => {
     console.log('deleted');
     event.target.parentNode.remove();
-    // console.log(this);
-    // $('.tasks-container').delete;
-    // $(event.target).
-    // text('This img was clicked.');
 });
 
-// $('.done').on('click', () => {
-//     $('ul').append('<li onclick= "lineThrough(this)">' + newToDo + '</li>');
-// });
 
 // done
 $('.tasks-container').on('click', '.done', (event) => {
     event.target.parentNode.style = 'text-decoration: line-through';
-    // console.log($(event.target));
-    // // ('.todo-text').style = 'text-decoration: line-through';
-    // // console.log($('.todo-text'));
-    // // console.log('done');
-    // // // $(event.target).
-    // // $(event.target).text('This img was clicked.');
 });
